f0e5a438e7cbd0000b836a4203606c925d9754a6
OMGboom7 <novaboom1029@gmail.com>
2023-06-03 14:27:24 +0800

    4

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+export default class CreateHomePage extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the create home page</p>
+    }
+}
